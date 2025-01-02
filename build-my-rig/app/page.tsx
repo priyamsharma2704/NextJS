279140c3146208979b4012a3bbd0350e2a7fa7cd
priyamsharma2704 <priyamsharma2704@gmail.com>
2025-01-01 19:50:28 -0600

    Build-My-Rig: added few routes

--- build-my-rig/app/page.tsx
+++ build-my-rig/app/page.tsx
@@ -1,39 +1,7 @@
 import Link from "next/link";
 export default function Home() {
-    const navigations = [
-        "Builder",
-        "Products",
-        "Guides",
-        "Completed Builds",
-        "Trends",
-        "Benchmarks",
-        "Forums",
-    ];
     return (
         <>
-            <nav>
-                <div className="text-4xl px-5 py-5">
-                    <span>BUILD</span>
-                    <span className="text-yellow-500">MY</span>
-                    <span>RIG</span>
-                </div>
-                <div className="flex text-xl border-solid border border-slate-700 w-screen">
-                    {navigations.map((nav) => (
-                        <Link
-                            href="/"
-                            className="p-5 hover:bg-slate-800 border-l boder-r border-slate-700"
-                        >
-                            {nav}
-                        </Link>
-                    ))}
-                    {/*ml-auto is to move the search input to the right */}
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="bg-slate-800 rounded-full ml-auto px-5 mt-3 mb-3 mr-2"
-                    />
-                </div>
-            </nav>
             {/* Start Your Build */}
             <div className="bg-[#282838] py-20 font-bold">
                 <p className="text-6xl flex justify-center ">
